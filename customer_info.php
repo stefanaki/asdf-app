d41f536b8a12b8f3ca7ff1732c8372630549c161
Giorgos Stefanakis <stefgs99@gmail.com>
2021-05-29 17:24:11 +0300

    New way of rendering data

--- customer_info.php
+++ customer_info.php
@@ -32,6 +32,10 @@
   $cust_result = mysqli_query($db, $cust_query);
   $customer = mysqli_real_escape_string($db, $_POST['customer']);
   $cust_data = "SELECT * FROM customer_data WHERE nfc_id = '$customer'";
+  $phone_query = "SELECT * FROM customer_phones WHERE customer_id = '$customer'";
+  $email_query = "SELECT * FROM customer_emails WHERE customer_id = '$customer'";
+  $phone_nums = mysqli_query($db, $phone_query);
+  $emails = mysqli_query($db, $email_query);
   ?>
 
     <form class="mb-3" action="customer_info.php" method="POST" style="margin: 0 25%">
@@ -41,7 +45,7 @@
             <option value="-1" selected>Select Customer</option>
             <?php while ($row = mysqli_fetch_row($cust_result)): ?>
               <option value="<?php echo htmlspecialchars($row[0]); ?>"><?php echo htmlspecialchars($row[1]); ?></option>
-            <?php endwhile; ?>
+            <?php $names[$row[0]] = $row[1]; endwhile; ?>
           </select>
         </div>
         <div class="col-4">
@@ -52,16 +56,18 @@
 
     <?php if (isset($_POST['select'])): ?>
       <?php $result = mysqli_query($db, $cust_data); $_SESSION['customer'] = $_POST['customer']; $sum = 0.0; ?>
+
+      <h4 class="mb-3" style="text-align: center"><?php echo $names[$_SESSION['customer']]; ?></h4>
       <table class="table table-striped table-hover border border-dark border-2 mx-auto mb-3" style="width: 50%; margin: 0 25%">
         <thead>
         <tr class="text-center">
           <th scope="col">NFC ID</th>
           <th scope="col">Name</th>
           <th scope="col">Verification ID</th>
+          <th scope="col">Date of Birth</th>
           <th scope="col">Age</th>
           <th scope="col">Gender</th>
-          <th scope="col">Phone Number(s)</th>
-          <th scope="col">e-mail(s)</th>
+          <th scope="col">Reserved Room</th>
         </tr>
       </thead>
       <tbody>
@@ -70,17 +76,46 @@
             <td><?php echo htmlspecialchars($row['nfc_id']); ?></td>
             <td><?php echo htmlspecialchars($row['first_name']) . " " . htmlspecialchars($row['last_name']); ?></td>
             <td><?php echo htmlspecialchars($row['verif_id']); ?></td>
+            <td><?php echo htmlspecialchars($row['birth']); ?></td>
             <td><?php echo htmlspecialchars($row['age']); ?></td>
             <td><?php echo htmlspecialchars($row['gender']); ?></td>
-            <td><?php echo htmlspecialchars($row['phone_number']); ?></td>
-            <td><?php echo htmlspecialchars($row['e-mail']); ?></td>
+            <td><?php echo htmlspecialchars($row['reserved']); ?></td>
           </tr>
         <?php endwhile; ?>
-        <tr class="text-center">
-        </tr>
-
       </tbody>
       </table>
+
+      <div class="d-flex flex-row text-center justify-content-center gap-5" style="margin: 0 25%">
+        <table class="table table-striped table-hover border border-dark border-2 mx-auto mb-3 flex-column">
+          <thead>
+            <tr>
+              <th>Phone Number(s)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php while ($row = $phone_nums->fetch_assoc()): ?>
+              <tr class="text-center">
+                <td><?php echo htmlspecialchars($row['phone_num']); ?></td>
+              </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
+        <table class="table table-striped table-hover border border-dark border-2 mx-auto mb-3 flex-column">
+          <thead>
+            <tr>
+              <th>e-mail(s)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php while ($row = $emails->fetch_assoc()): ?>
+              <tr class="text-center">
+                <td><?php echo htmlspecialchars($row['email']); ?></td>
+              </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
+      </div>
+
 
       <form class="mb-3" action="customer_info.php" method="POST" style="margin: 0 25%">
         <div class="mb-3 gap-2 mx-auto pt-2" style="float: right">
