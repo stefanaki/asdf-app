d10861b9d3768e6ba1c646533992cd862afc264f
Giorgos Stefanakis <stefgs99@gmail.com>
2021-06-11 18:30:52 +0300

    Index

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
     require_once("./templates/header.php");
     require_once("./db_connect.php");
   ?>
+
+<div class="container-md">
+  <h3 class="mb-3 mt-3 text-center">NTUA Databases Semestrial Project 2021</h3>
+  <div class="row mx-auto mb-3 pt-3" style="width: 55%">
+    <p class="col text-start"><b>Anna Douska</b></p>
+    <p class="col align-center" ><b>Odyssefs - Dimitrios Boufalis</b></p>
+    <p class="col text-end"><b>Georgios Stefanakis</b></p>
+  </div>
+
+<img src="assets/monet.jpg" alt="Claude Monet" class="border border-dark" style="width:60%; height:60%; display:block; margin-left: auto; margin-right: auto;">
+</div>
+
+
+
   <?php session_start(); ?>
   <?php require_once("./templates/footer.php"); ?>
 </html>
