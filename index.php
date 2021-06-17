2e5b045e0814da4a79b8020b6a072999888d37e5
Georgios Stefanakis <54993678+stefanaki@users.noreply.github.com>
2021-06-18 01:17:05 +0300

    Warning fix

--- index.php
+++ index.php
@@ -12,16 +12,13 @@
 <div class="container-md">
   <h3 class="mb-3 mt-3 text-center">NTUA Databases Semestrial Project 2021</h3>
   <div class="row mx-auto mb-3 pt-3" style="width: 55%">
-    <p class="col text-start"><b>Anna Douska</b></p>
-    <p class="col align-center" ><b>Odyssefs - Dimitrios Boufalis</b></p>
-    <p class="col text-end"><b>Georgios Stefanakis</b></p>
+    <p class="col text-start text-center">Anna Douska</p>
+    <p class="col align-center text-center">Odyssefs - Dimitrios Boufalis</p>
+    <p class="col text-end text-center">Georgios Stefanakis</p>
   </div>
 
 <img src="assets/monet.jpg" alt="Claude Monet" class="border border-dark" style="width:60%; height:60%; display:block; margin-left: auto; margin-right: auto;">
 </div>
 
-
-
-  <?php session_start(); ?>
   <?php require_once("./templates/footer.php"); ?>
 </html>
